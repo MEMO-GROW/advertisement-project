4cb9064e77ffaa0c81d82b6f82519c9cba116a22
MichaelAfedi <dmus0636@gmail.com>
2025-03-27 05:10:01 +0000

    Worked on Single page

--- src/pages/user/SingleAd.jsx
+++ src/pages/user/SingleAd.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import SingleAdsCard from '../../components/SingleAdsCard'
+import React, { useEffect, useState } from "react";
+import SingleAdsCard from "../../components/SingleAdsCard";
+import { useParams } from "react-router";
+import { apiGetSingleAd } from "../../services/adverts";
 
 const SingleAd = () => {
+  const { id } = useParams;
+
+  const [ad, setAd] = useState({});
+  const getAd = async () => {
+    try {
+      const response = await apiGetSingleAd(id);
+      setAd(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAd();
+  }, []);
   return (
     <div>
-      <SingleAdsCard/>
+      <SingleAdsCard />
     </div>
-  )
-}
+  );
+};
 
-export default SingleAd
+export default SingleAd;
